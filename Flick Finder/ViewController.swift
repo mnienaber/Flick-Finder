474fb1461ae9bbd2f035daf366135eae30d24a99
mnienaber <mnienaber@mnienaber-macbookpro3.roam.corp.google.com>
2016-02-09 17:22:11 +1100

    init

--- Flick Finder/ViewController.swift
+++ Flick Finder/ViewController.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
+let BASE_URL = "https://api.flickr.com/services/rest/"
+let METHOD_NAME = "flickr.photos.search"
+let API_KEY = "a2d1aaeead83f40edc51928ef2caf6a9"
+let GALLERY_ID = "66911286-72157647263150569"
+let EXTRAS = "url_m"
+let DATA_FORMAT = "json"
+let NO_JSON_CALLBACK = "1"
+let QUERY_STRING = "hydraglyph"
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageNameLabel: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var searchBoxText: UITextField!
+    @IBOutlet weak var latTextSearch: UITextField!
+    @IBOutlet weak var longTextSearch: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +36,121 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func textSearch(sender: AnyObject) {
+        print("yup")
+        
+        let methodArguments = [
+        "method": METHOD_NAME,
+        "api_key": API_KEY,
+        "text": QUERY_STRING,
+        "extras": EXTRAS,
+        "format": DATA_FORMAT,
+        "nojsoncallback": NO_JSON_CALLBACK]
+        
+        /* 3 - Initialize session and url */
+        let session = NSURLSession.sharedSession()
+        let urlString = BASE_URL + escapedParameters(methodArguments)
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+        
+        /* 5 - Create NSURLSessionDataTask and completion handler */
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? NSHTTPURLResponse {
+                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                } else if let response = response {
+                    print("Your request returned an invalid response! Response: \(response)!")
+                } else {
+                    print("Your request returned an invalid response!")
+                }
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            
+            /* Parse the data! */
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                parsedResult = nil
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            /* GUARD: Did Flickr return an error? */
+            guard let stat = parsedResult["stat"] as? String where stat == "ok" else {
+                print("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Are the "photos" and "photo" keys in our result? */
+            guard let photosDictionary = parsedResult["photos"] as? NSDictionary,
+                photoArray = photosDictionary["photo"] as? [[String: AnyObject]] else {
+                    print("Cannot find keys 'photos' and 'photo' in \(parsedResult)")
+                    return
+            }
+            
+            /* 7 - Generate a random number, then select a random photo */
+            let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+            let photoDictionary = photoArray[randomPhotoIndex] as [String: AnyObject]
+            let photoTitle = photoDictionary["title"] as? String /* non-fatal */
+            
+            /* GUARD: Does our photo have a key for 'url_m'? */
+            guard let imageUrlString = photoDictionary["url_m"] as? String else {
+                print("Cannot find key 'url_m' in \(photoDictionary)")
+                return
+            }
+            
+            /* 8 - If an image exists at the url, set the image and title */
+            let imageURL = NSURL(string: imageUrlString)
+            if let imageData = NSData(contentsOfURL: imageURL!) {
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.image.image = UIImage(data: imageData)
+                    self.imageNameLabel.text = imageUrlString
+                    print(imageUrlString)
+                    
+                })
+            } else {
+                print("Image does not exist at \(imageURL)")
+            }
+        }
+        
+        task.resume()
+    }
 
+    @IBAction func latLongSearch(sender: AnyObject) {
+    }
+    
+    func escapedParameters(parameters: [String : AnyObject]) -> String {
+        
+        var urlVars = [String]()
+        
+        for (key, value) in parameters {
+            
+            /* Make sure that it is a string value */
+            let stringValue = "\(value)"
+            
+            /* Escape it */
+            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+            
+            /* Append it */
+            urlVars += [key + "=" + "\(escapedValue!)"]
+            
+        }
+        return (!urlVars.isEmpty ? "?" : "") + urlVars.joinWithSeparator("&")
+    }
 }
 
