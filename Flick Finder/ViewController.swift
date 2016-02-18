0aae4311c67b3807ea92f323a111ce997f567d70
mnienaber <mnienaber@mnienaber-macbookpro3.roam.corp.google.com>
2016-02-18 23:29:48 +1100

    random page and image select - working

--- Flick Finder/ViewController.swift
+++ Flick Finder/ViewController.swift
@@ -79,7 +79,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
-        //print("Subscribe to the KeyboardWillShow and KeyboardWillHide notifications")
     }
     
     func unsubscribeToKeyboardNotifications() {
@@ -91,18 +90,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
     func keyboardWillShow(notification: NSNotification) {
         
         if view.frame.origin.y > 0 {
-            print("fine")
         } else {
             view.frame.origin.y -= getKeyboardHeight(notification) - 110
-            print(view.frame.origin.y)
         }
-        //print("Shift the view's frame up so that controls are shown")
     }
     
     func keyboardWillHide(notification: NSNotification) {
         
         view.frame.origin.y = 0
-        //print("Shift the view's frame down so that the view is back to its original placement")
     }
     
     func getKeyboardHeight(notification: NSNotification) -> CGFloat {
@@ -110,8 +105,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         let userInfo = notification.userInfo
         let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
         return keyboardSize.CGRectValue().height
-        //print("Get and return the keyboard's height from the notification")
-        //return 0.0
     }
     
     func escapedParameters(parameters: [String : AnyObject]) -> String {
@@ -143,7 +136,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
             
             latLongValue = ""
             return latLongValue!
-            
         } else if floatLatString > -91 && floatLatString < 91 && floatLongString > -181 && floatLongString < 181 {
             
             newLatString = floatLatString.description
@@ -158,18 +150,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }
     }
 
-    func searchQuery() {
-        
-        let methodArguments = [
-            "method": METHOD_NAME,
-            "api_key": API_KEY,
-            "text": self.searchBoxText.text!,
-            "bbox": buildBbox(),
-            "extras": EXTRAS,
-            "format": DATA_FORMAT,
-            //"per_page": PER_PAGE,
-            "nojsoncallback": NO_JSON_CALLBACK]
-        
+    func getImageBySearchQuery(methodArguments: [String: AnyObject]) {
         
         print("search text is \(searchBoxText.text!)")
         print("latlong text is \(self.latTextSearch.text! + ",0," + self.longTextSearch.text! + ",0")")
@@ -230,15 +211,89 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 return
             }
             
-            /* 2 - Determine the total number of photos */
-            /* GUARD: Is the "total" key in photosDictionary? */
-            guard let totalPhotos = (photosDictionary["total"] as? NSString)?.integerValue else {
-                print("Cannot find key 'total' in \(photosDictionary)")
+            /* GUARD: Is "pages" key in the photosDictionary? */
+            guard let totalPages = photosDictionary["pages"] as? Int else {
+                print("Cannot find key 'pages' in \(photosDictionary)")
                 return
             }
             
-            /* 3 - If photos are returned, let's grab one! */
-            if totalPhotos > 0 {
+            /* Flickr API will only return up the 4000 images (100 per page * 40 page max) */
+            /* Pick a random page! */
+            let pageLimit = min(totalPages, 40)
+            let randomPage = Int(arc4random_uniform(UInt32(pageLimit))) + 1
+            self.getImageBySearchQueryWithPage(methodArguments, pageNumber: randomPage)
+        }
+        task.resume()
+    }
+    
+    func getImageBySearchQueryWithPage(methodArguments: [String: AnyObject], pageNumber: Int) {
+        
+        var withPageDictionary = methodArguments
+        withPageDictionary["page"] = pageNumber
+        
+        let session = NSURLSession.sharedSession()
+        let urlString = BASE_URL + escapedParameters(methodArguments)
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+        print(urlString)
+        
+        /* 5 - Create NSURLSessionDataTask and completion handler */
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? NSHTTPURLResponse {
+                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                } else if let response = response {
+                    print("Your request returned an invalid response! Response: \(response)!")
+                } else {
+                    print("Your request returned an invalid response!")
+                }
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            
+            /* Parse the data! */
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                parsedResult = nil
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            /* GUARD: Did Flickr return an error? */
+            guard let stat = parsedResult["stat"] as? String where stat == "ok" else {
+                print("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            /* 1 - Get the photos dictionary */
+            /* GUARD: Is "photos" key in our result? */
+            guard let photosDictionary = parsedResult["photos"] as? NSDictionary else {
+                print("Cannot find keys 'photos' in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Is "pages" key in the photosDictionary? */
+            guard let totalPagesVal = photosDictionary["pages"] as? Int else {
+                print("Cannot find key 'pages' in \(photosDictionary)")
+                return
+            }
+            
+            if totalPagesVal > 0 {
                 
                 /* GUARD: Is the "photo" key in photosDictionary? */
                 guard let photosArray = photosDictionary["photo"] as? [[String: AnyObject]] else {
@@ -249,8 +304,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 /* 4 - Get a random index, and pick a random photo's dictionary */
                 let randomPhotoIndex = Int(arc4random_uniform(UInt32(photosArray.count)))
                 let photoDictionary = photosArray[randomPhotoIndex] as [String: AnyObject]
-                
-                /* 5 - Prepare the UI updates */
                 let photoTitle = photoDictionary["title"] as? String /* non-fatal */
                 
                 /* GUARD: Does our photo have a key for 'url_m'? */
@@ -266,30 +319,71 @@ class ViewController: UIViewController, UITextFieldDelegate {
                         self.image.image = UIImage(data: imageData)
                         self.imageNameLabel.text = photoTitle
                         print(imageUrlString)
+                        
+                        if methodArguments["bbox"] != nil {
+                            if let photoTitle = photoTitle {
+                                self.imageNameLabel.text = "\(self.latLongValue) \(photoTitle)"
+                            } else {
+                                self.imageNameLabel.text = "nothing"
+                            }
+                        } else {
+                            self.imageNameLabel.text = photoTitle ?? "(Untitled)"
+                            
+                        }
                     })
                 } else {
-                    dispatch_async(dispatch_get_main_queue(), {
-                        self.image.image = UIImage(named: "dark")
-                        self.imageNameLabel.text = "Got nothing for ya, try a different search!"
-                    })
+                    print("Image does not exist at \(imageURL)")
                 }
             } else {
-                print("No photos here!")
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.image.image = nil
+                    self.imageNameLabel.text = "Got nothing for ya, try a different search!"
+                })
             }
         }
         handleSingleTap(tap!)
         task.resume()
     }
 
+
     @IBAction func textSearch(sender: AnyObject) {
         
-        searchQuery()
+        self.dismissAnyVisibleKeyboards()
+        
+        let methodArguments: [String: String!] = [
+            "method": METHOD_NAME,
+            "api_key": API_KEY,
+            "text": self.searchBoxText.text!,
+            "extras": EXTRAS,
+            "format": DATA_FORMAT,
+            "nojsoncallback": NO_JSON_CALLBACK
+        ]
+        getImageBySearchQuery(methodArguments)
     }
     
     @IBAction func latLongSearch(sender: AnyObject) {
         
-        searchQuery()
+        self.dismissAnyVisibleKeyboards()
+        
+        let methodArguments = [
+            "method": METHOD_NAME,
+            "api_key": API_KEY,
+            //"text": self.searchBoxText.text!,
+            "bbox": buildBbox(),
+            "extras": EXTRAS,
+            "format": DATA_FORMAT,
+            "nojsoncallback": NO_JSON_CALLBACK
+        ]
+        getImageBySearchQuery(methodArguments)
     }
-    
 }
+
+extension ViewController {
+    func dismissAnyVisibleKeyboards() {
+        if searchBoxText.isFirstResponder() || latTextSearch.isFirstResponder() || longTextSearch.isFirstResponder() {
+            self.view.endEditing(true)
+        }
+    }
+}
+
 
